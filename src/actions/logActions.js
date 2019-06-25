e33662d6a85b389d2743db2c7645a292b6fa51c0
skin93 <arro.ciesielski@gmail.com>
2019-06-25 21:17:50 +0200

    redux connected to component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -14,6 +14,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 //   };
 // };
 
+// get logs fro mserver
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
